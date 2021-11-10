e15600fc0ed66f6a83a1b254f0500824c6fadf65
Carolina Guevara <USIS037621@ugb.edu.sv>
2021-11-09 18:48:05 -0600

    Formulario ayuda

---
+++ MedSystemWeb/WebForm4.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm4.aspx.cs" Inherits="MedSystemWeb.WebForm4" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:TextBox ID="TextBox1" runat="server" BackColor="White" Height="266px" TextMode="MultiLine" Width="909px"></asp:TextBox>
+</asp:Content>
+
---
+++ MedSystemWeb/WebForm4.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MedSystemWeb
+{
+    public partial class WebForm4 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            StreamReader Archivo = new StreamReader("c:\\ayuda.txt");
+            string linea = "";
+            ArrayList Contenido = new ArrayList();
+        }
+
+            while (Linea != null)
+        {
+                linea = Archivo.Readline();
+                 if (linea !=null)
+                  contenido.Add(Linea);
+             texto.Text = Linea; 
+         }
+    Archivo.Close();
+               texto.Text = "";
+        foreach (String linea_mostrar in contenido)
+        {
+           texto.Text = texto.Text + linea_mostrar + "String.Chr(13);
+        }
+    }
+}
